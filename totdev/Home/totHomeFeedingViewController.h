6a9b8dc94b2265ba554b5bf52708b6f4d4c41666
Yifei Chen <cyf@guest-wireless-207-151-236-092.usc.edu>
2012-05-06 11:27:16 -0700

    add feeding view

---
+++ totdev/Home/totHomeFeedingViewController.h
@@ -0,0 +1,13 @@
+//
+//  totHomeFeedingViewController.h
+//  totdev
+//
+//  Created by Yifei Chen on 5/6/12.
+//  Copyright (c) 2012 USC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface totHomeFeedingViewController : UIViewController
+
+@end
