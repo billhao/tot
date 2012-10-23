8025d03ac30faa39f6aaddb37a1d36921cbd2484
Lixing Huang <huanglx1984@hotmail.com>
2012-10-22 23:55:09 -0700

    story description

--- totdev/Review/totReviewStoryView.m
+++ totdev/Review/totReviewStoryView.m
@@ -7,6 +7,9 @@
 //
 
 #import "totReviewStoryView.h"
+#import "AppDelegate.h"
+#import "../Model/totModel.h"
+#import "../Model/totEvent.h"
 
 @implementation totReviewStoryView
 
@@ -18,7 +21,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        [self setBackgroundColor:[UIColor grayColor]];
+        [self setBackgroundColor:[UIColor whiteColor]];
         
         height = frame.size.height;
         width  = frame.size.width;
@@ -55,15 +58,91 @@
 }
 
 // get the story description
+- (BOOL) isFirstOccurrence:(NSString*)event_type forBaby:(int)babyId inDatabase:(totModel*)db {
+    NSArray* events = [db getEvent:babyId event:event_type];
+    if ([events count] == 1) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
 - (NSString*)getStoryDescription:(totReviewStory*)story {
     NSString* desc = nil;
     NSArray* tokens = [story.mEventType componentsSeparatedByString:@"/"];
     NSString* category = [tokens objectAtIndex:0];
+    NSString* rawValue = story.mRawContent;
+    
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    totModel* database = [appDelegate getDataModel];
+    
     if ([category isEqualToString:@"basic"]) {
         NSString* subcategory = [tokens objectAtIndex:1];
+        
         if ([subcategory isEqualToString:@"diaper"]) {
-            NSString* rawValue = story.mRawContent;
-            desc = [NSString stringWithFormat:@"%s", [rawValue UTF8String]];
+            desc = [NSString stringWithFormat:@"has a %s diaper", [rawValue UTF8String]];
+        } else if ([subcategory isEqualToString:@"language"]) {
+            desc = [NSString stringWithFormat:@"said %s", [rawValue UTF8String]];
+        } else if ([subcategory isEqualToString:@"sleep"]) {
+            if ([rawValue isEqualToString:@"end"]) {
+                
+            }
+        } else if ([subcategory isEqualToString:@"height"]) {
+            desc = [NSString stringWithFormat:@"is %s now", [rawValue UTF8String]];
+        } else if ([subcategory isEqualToString:@"weight"]) {
+            desc = [NSString stringWithFormat:@"is %s now", [rawValue UTF8String]];
+        } else if ([subcategory isEqualToString:@"head"]) {
+            desc = [NSString stringWithFormat:@"is %s now", [rawValue UTF8String]];
+        }
+    } else if ([category isEqualToString:@"eye_contact"]) {
+        if ([self isFirstOccurrence:category forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = @"first eye contact";
+        } else {
+            desc = @"has an eye contact";
+        }
+    } else if ([category isEqualToString:@"vision_attention"]) {
+        if ([self isFirstOccurrence:category forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = @"first vision attention";
+        } else {
+            desc = @"has a vision attention";
+        }
+    } else if ([category isEqualToString:@"chew"]) {
+        if ([self isFirstOccurrence:category forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = @"first chew";
+        } else {
+            desc = @"chewed";
+        }
+    } else if ([category isEqualToString:@"mirror_test"]) {
+        if ([self isFirstOccurrence:category forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = @"first mirror test";
+        } else {
+            desc = @"has a mirror test";
+        }
+    } else if ([category isEqualToString:@"imitation"]) {
+        if ([self isFirstOccurrence:category forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = @"first imitation";
+        } else {
+            desc = @"has an imitation";
+        }
+    } else if ([category isEqualToString:@"motor_skill"]) {
+        NSString* subcategory = [tokens objectAtIndex:1];
+        if ([self isFirstOccurrence:story.mEventType forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = [NSString stringWithFormat:@"first %s", [subcategory UTF8String]];
+        } else {
+            desc = [NSString stringWithFormat:@"%s", [subcategory UTF8String]];
+        }
+    } else if ([category isEqualToString:@"emotion"]) {
+        NSString* subcategory = [tokens objectAtIndex:1];
+        if ([self isFirstOccurrence:story.mEventType forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = [NSString stringWithFormat:@"first %s", [subcategory UTF8String]];
+        } else {
+            desc = [NSString stringWithFormat:@"%s", [subcategory UTF8String]];
+        }
+    } else if ([category isEqualToString:@"gesture"]) {
+        NSString* subcategory = [tokens objectAtIndex:1];
+        if ([self isFirstOccurrence:story.mEventType forBaby:appDelegate.mBabyId inDatabase:database]) {
+            desc = [NSString stringWithFormat:@"first %s", [subcategory UTF8String]];
+        } else {
+            desc = [NSString stringWithFormat:@"%s", [subcategory UTF8String]];
         }
     }
     return desc;
@@ -90,7 +169,15 @@
         [icon release];
     }
     
-    
+    NSString *description = [self getStoryDescription:story];
+    if (description) {
+        UILabel *storyDesc = [[UILabel alloc] initWithFrame:CGRectMake(50, -15, 200, 50)];
+        storyDesc.backgroundColor = [UIColor clearColor];
+        storyDesc.text = description;
+        [storyDesc setFont:[UIFont fontWithName:@"verdana" size:13]];
+        [self addSubview:storyDesc];
+        [storyDesc release];
+    }
 }
 
 - (void)setReviewStory:(totReviewStory *)story {
