433a906f9ae18e9c45ea4cf911a685fb0c0f3628
Chengjie Zhang <chengjie@chengjie-pro.local>
2012-10-25 16:21:08 -0700

    working on feeding page

--- totdev/Home/totHomeFeedingViewController.m
+++ totdev/Home/totHomeFeedingViewController.m
@@ -104,8 +104,6 @@
     [self.view addSubview:background];
     [background release];
     
-    
-    
     // create categories slider view
     mCategoriesSlider = [[totSliderView alloc] initWithFrame:CGRectMake(38, 170, 244, 120)];
     [mCategoriesSlider setDelegate:self];
@@ -133,7 +131,6 @@
     [self.view addSubview:mCategoriesSlider];
     
     buttonSelected = 0;
-
     
     // create recently used slider view
     mRecentlyUsedSlider = [[totSliderView alloc] initWithFrame:CGRectMake(38, 95, 244, 60)];
@@ -175,43 +172,22 @@
     [mNavigationBar setDelegate:self];
     [self.view addSubview:mNavigationBar];
     
-    //create picker for oz
-    picker_quantity = [[STHorizontalPicker alloc] initWithFrame:CGRectMake(0, 265, 320, 31)];
-    picker_quantity.name = @"picker_weight";
-    [picker_quantity setMinimumValue:0.0];
-    [picker_quantity setMaximumValue:6.0];
-    [picker_quantity setSteps:60];
-    [picker_quantity setDelegate:self];
-    [picker_quantity setValue:DEFAULT_QUANTITY];
-    //diable picker quantity
-    picker_quantity.hidden =YES;
-    
-    [self.view addSubview:picker_quantity];
-    
-    //create ok button
-    mOKButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    mOKButton.frame = CGRectMake(150, 347, 160, 40);
-    [mOKButton setTitle:@"OK" forState:UIControlStateNormal];
+    //create ok button from icons
+
+    mOKButton = [UIButton buttonWithType:UIButtonTypeCustom];;
+    mOKButton.frame = CGRectMake(220, 360, 40, 40);
+    [mOKButton setImage:[UIImage imageNamed:@"icons-ok.png"] forState:UIControlStateNormal];
     [self.view addSubview:mOKButton];
-    
-    mDatetime = [UIButton buttonWithType:UIButtonTypeCustom];
-    mDatetime.frame = CGRectMake(10, 347, 130, 40);
-    [mDatetime setTitle:[totUtility nowTimeString] forState:UIControlStateNormal];
-    [self.view addSubview:mDatetime];  
-    
-    mSummary = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    mSummary.frame = CGRectMake(50, 80, 220, 311);
-    [mSummary setHidden:YES];
-    mSummary.titleLabel.lineBreakMode=UILineBreakModeWordWrap;
-    //mSummary.titleLabel.frame= mSummary.bounds;
-    [self.view addSubview:mSummary];  
-
-    
-    // set up events
     [mOKButton addTarget:self action:@selector(OKButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //date and time
+    mDatetime = [UIButton buttonWithType:UIButtonTypeCustom];
+    mDatetime.frame = CGRectMake(100, 40, 120, 30);
+    [mDatetime setTitle:[totUtility nowTimeString] forState:UIControlStateNormal];
+    [self.view addSubview:mDatetime];
     [mDatetime addTarget:self action:@selector(DatetimeClicked:) forControlEvents:UIControlEventTouchUpInside];
-    [mSummary addTarget:self action:@selector(SummaryButtonClicked:) forControlEvents: UIControlEventTouchUpInside];
-
+    [mDatetime setBackgroundColor:[UIColor whiteColor]];
+    [mDatetime setTitleColor:[UIColor magentaColor] forState:UIControlStateNormal];
     
     // set up date picker
     mWidth = self.view.frame.size.width;
@@ -223,10 +199,47 @@
     [mClock setDelegate:self];
     [mClock setCurrentTime];
     [self.view addSubview:mClock.view];
+
+    //final summary popup
+    mSummary = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    mSummary.frame = CGRectMake(50, 80, 220, 311);
+    [mSummary setHidden:YES];
+    mSummary.titleLabel.lineBreakMode=UILineBreakModeWordWrap;
+    //mSummary.titleLabel.frame= mSummary.bounds;
+    [self.view addSubview:mSummary];
+    [mSummary addTarget:self action:@selector(SummaryButtonClicked:) forControlEvents: UIControlEventTouchUpInside];
     
-    for (int i=0; i<DEFAULT_MENU ; i++) {
-        quantityList[i]=0;    
-    }
+    
+    //add a chosen list slider
+    // create recently used slider view
+    mFoodChosenSlider = [[totSliderView alloc] initWithFrame:CGRectMake(38, 285, 244, 60)];
+    [mFoodChosenSlider setDelegate:self];
+    [mFoodChosenSlider setBtnPerCol:1];
+    [mFoodChosenSlider setBtnPerRow:4];
+    [mFoodChosenSlider setvMarginBetweenBtn:0];
+    [mFoodChosenSlider sethMarginBetweenBtn:0];
+    //[mRecentlyUsedSlider setBtnHeight:40];
+     
+    //load image
+    NSMutableArray *foodChosenImages = [[NSMutableArray alloc] init];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-apple.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-blueberry.png"]];
+    
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-papaya.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-kiwi.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-mango.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-bread.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-banana.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-whiterice.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-cheerios.png"]];
+    [foodChosenImages addObject:[UIImage imageNamed:@"feedinggrey-pear.png"]];
+    
+    [mFoodChosenSlider setContentArray:foodChosenImages];
+    [foodChosenImages release];
+    [mFoodChosenSlider getWithPositionMemoryIdentifier:@"feedingFoodChosen"];
+    [self.view addSubview:mFoodChosenSlider];
+     
+
 }
 
 
